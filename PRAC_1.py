b057faed5ffb501eb9cd28c908519ddf6afb229c
Ashishraorane <71246957+Ashishraorane@users.noreply.github.com>
2020-09-14 10:53:09 +0530

    This it the telegram bot

---
+++ PRAC_1.py
@@ -0,0 +1,17 @@
+import telepot
+token = '1204142141:AAE5ZRnL4YLXpOkgbtR5D-ncKNXM3iRJN64'
+TelegramBot = telepot.Bot(token)
+print (TelegramBot.getMe())
+
+
+https://api.telegram.org/1204142141:AAE5ZRnL4YLXpOkgbtR5D-ncKNXM3iRJN64/getMe
+
+https://api.telegram.org/1204142141:AAE5ZRnL4YLXpOkgbtR5D-ncKNXM3iRJN64/getUpdates
+
+Id: 1374937435
+
+https://api.telegram.org/1204142141:AAE5ZRnL4YLXpOkgbtR5D-ncKNXM3iRJN64/
+
+https://api.telegram.org/1204142141:AAE5ZRnL4YLXpOkgbtR5D-ncKNXM3iRJN64/sendMessage?chat_id=1113753715
+
+https://api.telegegram.org/1204142141:AAE5ZRnL4YLXpOkgbtR5D-ncKNXM3iRJN64/sendMessage?chat_id=1113753715&text=Hello Vishal

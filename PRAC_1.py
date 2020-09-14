310c4cb0ba045cf3f479fce4f122590749c5de2f
Ashishraorane <71246957+Ashishraorane@users.noreply.github.com>
2020-09-14 11:14:34 +0530

    Update PRAC_1.py

--- PRAC_1.py
+++ PRAC_1.py
@@ -4,14 +4,14 @@ TelegramBot = telepot.Bot(token)
 print (TelegramBot.getMe())
 
 
-https://api.telegram.org/1204142141:AAE5ZRnL4YLXpOkgbtR5D-ncKNXM3iRJN64/getMe
+https://api.telegram.org/bot1204142141:1204142141:AAE5ZRnL4YLXpOkgbtR5D-ncKNXM3iRJN64/getMe
 
-https://api.telegram.org/1204142141:AAE5ZRnL4YLXpOkgbtR5D-ncKNXM3iRJN64/getUpdates
+https://api.telegram.org/bot1204142141:1204142141:AAE5ZRnL4YLXpOkgbtR5D-ncKNXM3iRJN64/getUpdates
 
 Id: 1374937435
 
-https://api.telegram.org/1204142141:AAE5ZRnL4YLXpOkgbtR5D-ncKNXM3iRJN64/
+https://api.telegram.org/bot1204142141:AAE5ZRnL4YLXpOkgbtR5D-ncKNXM3iRJN64/
 
-https://api.telegram.org/1204142141:AAE5ZRnL4YLXpOkgbtR5D-ncKNXM3iRJN64/sendMessage?chat_id=1113753715
+https://api.telegram.org/bot1204142141:AAE5ZRnL4YLXpOkgbtR5D-ncKNXM3iRJN64/sendMessage?chat_id=1113753715
 
-https://api.telegegram.org/1204142141:AAE5ZRnL4YLXpOkgbtR5D-ncKNXM3iRJN64/sendMessage?chat_id=1113753715&text=Hello Vishal
+https://api.telegegram.org/bot1204142141:AAE5ZRnL4YLXpOkgbtR5D-ncKNXM3iRJN64/sendMessage?chat_id=1113753715&text=Hello Vishal
